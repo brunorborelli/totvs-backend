dbafeb2c25a6878eaf57233c7cf14f52acb2fd0c
Bruno Borelli <rodriguesborellibruno@gmail.com>
2024-06-17 12:34:05 -0300

    #09[fix] - validacoes telefone e nome

--- src/main/java/com/totvs/backend/repository/TelefoneRepository.java
+++ src/main/java/com/totvs/backend/repository/TelefoneRepository.java
@@ -2,6 +2,12 @@ package com.totvs.backend.repository;
 
 import com.totvs.backend.model.telefone.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+
+    @Query(value = "SELECT * FROM telefone WHERE telefone = ?1 AND status = true", nativeQuery = true)
+    List<Telefone> findByTelefone(String telefone);
 }
